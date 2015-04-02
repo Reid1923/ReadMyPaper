0b5a72bd08a43fd2708d0a01216f5bcce9aaa0ed
Christine Boyle <christine.boyle15@gmail.com>
2015-04-01 20:20:38 -0400

    You can now login, logout, and notification when incorrect user and password (still need account registration)

--- ReadMyPaper/ReadMyPaper/settings.py
+++ ReadMyPaper/ReadMyPaper/settings.py
@@ -11,6 +11,12 @@ https://docs.djangoproject.com/en/1.7/ref/settings/
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 import os
 BASE_DIR = os.path.dirname(os.path.dirname(__file__))
+PROJECT_PATH = os.path.dirname(os.path.realpath(__file__))
+
+TEMPLATE_DIRS = (
+
+    os.path.join(PROJECT_PATH, '../templates/accounts') ,
+)
 
 
 # Quick-start development settings - unsuitable for production
@@ -36,6 +42,7 @@ INSTALLED_APPS = (
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    #'registration' ,
 )
 
 MIDDLEWARE_CLASSES = (
--- ReadMyPaper/ReadMyPaper/urls.py
+++ ReadMyPaper/ReadMyPaper/urls.py
@@ -7,4 +7,13 @@ urlpatterns = patterns('',
     # url(r'^blog/', include('blog.urls')),
 
     url(r'^admin/', include(admin.site.urls)),
-)
+
+
+ #user auth urls
+
+ 	url(r'^accounts/login/$' , 'ReadMyPaper.views.login') ,
+ 	url(r'^accounts/auth/$' , 'ReadMyPaper.views.auth_view') ,
+ 	url(r'^accounts/logout/$' , 'ReadMyPaper.views.logout') , 
+ 	url(r'^accounts/loggedin/$' , 'ReadMyPaper.views.loggedin') ,
+ 	url(r'^accounts/invalid/$' , 'ReadMyPaper.views.invalid_login') ,
+ )
