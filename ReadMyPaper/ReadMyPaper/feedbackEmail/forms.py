427b333e8fb17eb3bdd2cd06cfc67a58d9f4ec06
Christine Boyle <christine.boyle15@gmail.com>
2015-04-30 15:56:31 -0400

    final product

--- ReadMyPaper/ReadMyPaper/feedbackEmail/forms.py
+++ ReadMyPaper/ReadMyPaper/feedbackEmail/forms.py
@@ -2,7 +2,7 @@ from django import forms
 
 class EmailForm(forms.Form):
     email = forms.EmailField()
-    subject = forms.CharField(max_length=100)
+    #subject = forms.CharField(max_length=100)
     attach = forms.Field(widget = forms.FileInput)
-    message = forms.CharField(widget = forms.Textarea)
+    #message = forms.CharField(widget = forms.Textarea)
 
--- ReadMyPaper/ReadMyPaper/feedbackEmail/views.py
+++ ReadMyPaper/ReadMyPaper/feedbackEmail/views.py
@@ -10,11 +10,13 @@ def send_email(request):
     if request.method != 'POST':
         form = EmailForm()
         return render(request ,'email.html', {'email_form': form})
-	
-    form = EmailForm(request.POST, request.FILES)	
+    
+    form = EmailForm(request.POST, request.FILES)   
     if form.is_valid():
-        subject = form.cleaned_data['subject']
-        message = form.cleaned_data['message']
+        subject = "Your feedback from Read My Paper is ready"
+        
+        message = "Hello there, \n Your feedback from Read My Paper is ready and downloadable in the attachment. Please do not forget to be courteous and make edits for others! \n -The Read My Paper team"
+        
         email = form.cleaned_data['email']
         attach = request.FILES['attach']
         ext = ['.doc' , '.docx' , '.txt' , '.pdf' , '.rtf']
@@ -26,4 +28,4 @@ def send_email(request):
             return render(request ,'email.html', {'message': 'Sent email to %s'%email})   
         
         return render(request , 'email.html' , {'message': 'Please attach a file that ends with one of the following: .doc , .docx , .txt , .pdf , .rtf'})
- #           return render(request ,'email.html' , {'message': 'Did not send email to %s'%email})	
+ #           return render(request ,'email.html' , {'message': 'Did not send email to %s'%email})
